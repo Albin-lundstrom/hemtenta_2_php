ddeb93e2f9583fb89532982c705408f16a02a89a
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2023-11-06 09:13:03 +0100

    first commit

---
+++ php/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hemtenta_2</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/bootstrap.min.js" defer></script>
+    <!-- <srcipt src="./js/app.js" defer></sricpt> -->
+</head>
+<body>
+    <div class="d-flex flex-row bd-highlight mb-3 justify-content-center">
+        <div class="p-2 bd-highlight">
+            <h3 class="">
+                Lägg till produkt
+            </h3>
+        </div>
+        <div class="p-2 bd-highlight">
+            <h3>
+                Se alla produkter
+            </h3>
+        </div>
+        <div class="p-2 bd-highlight">
+            <h3>
+                Ändra pris/bild på produkt
+            </h3>
+        </div>
+        <div class="p-2 bd-highlight">
+            <h3>
+                Ta bort produkt
+            </h3>
+        </div>
+    </div>
+</body>
+</html>
